ef69e3d990003422690250daac562a5f724cc763
tchungnz <tchung.nz@gmail.com>
2016-07-19 16:05:10 +0100

    Redirected the post names to a session and get request

--- app.rb
+++ app.rb
@@ -2,14 +2,22 @@ require 'sinatra/base'
 require 'shotgun'
 
 class Battle < Sinatra::Base
+
+  enable :sessions
+
   get '/' do
-    'Hello Battle!'
     erb :index
   end
 
   post '/names' do
-    @player_1_name = params[:player_1_name]
-    @player_2_name = params[:player_2_name]
+    session[:player_1_name] = params[:player_1_name]
+    session[:player_2_name] = params[:player_2_name]
+    redirect '/play'
+  end
+
+  get '/play' do
+    @player_1_name = session[:player_1_name]
+    @player_2_name = session[:player_2_name]
     erb :play
   end
 
